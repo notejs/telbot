9e4fe1d6f498b5014590124177ebc6e401325dcb
Linus Wang <youjiang.wang@okcoin.com>
2019-12-27 17:52:41 +0800

    init

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const Telegraf = require('telegraf')
 const bot = new Telegraf('985672071:AAHJXvPK1EUuPl9Nxv1evMDXCCcLKUC8qzY')
 
 
-bot.use((ctx) => {
+bot.use((ctx, next) => {
     // console.log(ctx.message)
+    return next();
 })
 
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
-bot.command('okex', Telegraf.reply('https://www.okex.com'))
+bot.command('okex', Telegraf.reply('https://www.okex.com'));
 
 
-bot.command('p', async (ctx) => {
-    console.log(ctx.message, ctx.chat);
-    const { reply } = ctx;
-    reply('dddd');
+bot.command('p', (ctx) => {
+    console.log(ctx.message);
+    ctx.reply('dddd');
 })
 
-bot.launch()
+bot.launch();
